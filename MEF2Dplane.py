c4a81947829dc6ee84215ec26e03ec6630110ac4
Marcos Arndt <arndt@ufpr.br>
2026-02-20 11:58:13 -0300

    Add files via upload

--- MEF2Dplane.py
+++ MEF2Dplane.py
@@ -14,6 +14,114 @@ def constitutiva(tpproblema,E,ni):
         De = E/((1+ni)*(1-2*ni)) * np.array([[1-ni,ni,0],[ni,1-ni,0],[0,0,(1-2*ni)/2]])
     return(De)
 
+# Matriz de rigidez para elementos T3
+def rigT3():
+    for i in range(nelem):
+        D = constitutiva(dados_analise[0],Mat[Inci[i,0],1],Mat[Inci[i,0],2])
+        n1 = Inci[i,1]
+        n2 = Inci[i,2]
+        n3 = Inci[i,3]
+        Ae = 1/2.*(Coord[n2,0]*Coord[n3,1]+Coord[n1,0]*Coord[n2,1]+Coord[n3,0]*Coord[n1,1]-Coord[n2,0]*Coord[n1,1]
+                   -Coord[n1,0]*Coord[n3,1]-Coord[n3,0]*Coord[n2,1])
+        if Ae < 0: 
+            print(f'Elemento {i}: Ae = {Ae}')
+            parada = input()
+        b = np.array([Coord[n2,1]-Coord[n3,1],(-1)*Coord[n1,1]+Coord[n3,1],Coord[n1,1]-Coord[n2,1]])
+        c = np.array([(-1)*Coord[n2,0]+Coord[n3,0],Coord[n1,0]-Coord[n3,0],(-1)*Coord[n1,0]+Coord[n2,0]])
+        B = 1/(2*Ae)*np.array([[b[0],0.,b[1],0.,b[2],0.],[0.,c[0],0.,c[1],0.,c[2]],[c[0],b[0],c[1],b[1],c[2],b[2]]])
+        list_B.append(B)
+        if dados_analise[0] == 'EPT':
+            t = Mat[Inci[i,0],0]
+        else:
+            t = 1.
+        Ke = t*Ae*np.dot(np.dot(np.transpose(B),D),B)
+        pos = [2*n1,2*n1+1,2*n2,2*n2+1,2*n3,2*n3+1]
+        list_pos.append(pos)
+        for j in range(6):
+            for k in range(6):
+                Kg[pos[j],pos[k]] += Ke[j,k]
+    return(Kg,list_B,list_pos)
+
+# Matriz de rigidez para elementos Q4 : quadratura de Gauss com 2 pontos em cada direção
+def rigQ4():
+    # pontos de Gauss
+    nptg = 4
+    ptg = [[-1./(3.**0.5),-1./(3.**0.5)],[-1./(3.**0.5),1./(3.**0.5)],[1./(3.**0.5),-1./(3.**0.5)],[1./(3.**0.5),1./(3.**0.5)]]
+    # pesos de Gauss
+    peso = [[1.,1.],[1.,1.],[1.,1.],[1.,1.]]
+    for i in range(nelem):
+        D = constitutiva(dados_analise[0],Mat[Inci[i,0],1],Mat[Inci[i,0],2])
+        n1 = Inci[i,1]
+        n2 = Inci[i,2]
+        n3 = Inci[i,3]
+        n4 = Inci[i,4]
+        cnos = np.array([[Coord[n1,0],Coord[n1,1]],
+                          [Coord[n2,0],Coord[n2,1]],
+                          [Coord[n3,0],Coord[n3,1]],
+                          [Coord[n4,0],Coord[n4,1]]])
+        if dados_analise[0] == 'EPT':
+            t = Mat[Inci[i,0],0]
+        else:
+            t = 1.
+        Ke = np.zeros((8,8))
+        for j in range(nptg):
+            xi = ptg[j][0]
+            eta = ptg[j][1]
+            w1 = peso[j][0]
+            w2 = peso[j][1]
+            dxi = np.array([[1./4.*(1-eta), 1./4.*(1+eta), -1./4.*(1+eta), -1./4.*(1-eta)],
+                         [-1./4.*(1+xi), 1./4.*(1+xi), 1./4.*(1-xi), -1./4.*(1-xi)]])
+            J = np.dot(dxi,cnos)
+            detJ = J[0,0]*J[1,1] - J[0,1]*J[1,0]
+            invJ = 1/detJ * np.array([[J[1,1],-J[0,1]],
+                                      [-J[1,0],J[0,0]]])
+            dx = np.dot(invJ,dxi)
+            B = np.array([[dx[0,0],0,dx[0,1],0,dx[0,2],0,dx[0,3],0],
+                          [0,dx[1,0],0,dx[1,1],0,dx[1,2],0,dx[1,3]],
+                          [dx[1,0],dx[0,0],dx[1,1],dx[0,1],dx[1,2],dx[0,2],dx[1,3],dx[0,3]]])
+            F = t * detJ * np.dot(np.transpose(B),np.dot(D,B))
+            Ke += w1*w2*F
+        pos = [2*n1,2*n1+1,2*n2,2*n2+1,2*n3,2*n3+1,2*n4,2*n4+1]
+        list_pos.append(pos)
+        for j in range(8):
+            for k in range(8):
+                Kg[pos[j],pos[k]] += Ke[j,k]
+    return(Kg,list_pos)
+
+# Cálculo das tensões nodais nos elementos CST (T3) (média dos elementos que possuem o nó)
+def stressT3():
+    print('Tensões nos elementos:')
+    list_S = []
+    for i in range(nelem):
+        posel = list_pos[i]
+        ue = np.array([d[posel[0]],d[posel[1]],d[posel[2]],d[posel[3]],d[posel[4]],d[posel[5]]])
+        D = constitutiva(dados_analise[0],Mat[Inci[i,0],1],Mat[Inci[i,0],2])
+        B = list_B[i]
+        S = np.dot(D,np.dot(B,ue))
+        list_S.append(S)
+        print(f'Elemento {i}:')
+        print(f'Sx = {S[0]}')
+        print(f'Sy = {S[1]}')
+        print(f'Sxy = {S[2]}')
+        for j in range(Inci.shape[1]-1):
+            index_nos[Inci[i,j+1]].append(i)
+    # Tensões médias nos nós para elemento T3
+    Sx = np.zeros((nnos))
+    Sy = np.zeros((nnos))
+    Sxy = np.zeros((nnos))
+    for i in range(nnos):
+        S = np.zeros((3))
+        for j in index_nos[i]:
+            S += list_S[j]
+        S = 1./len(index_nos[i]) * S
+        Sx[i] = S[0]
+        Sy[i] = S[1]
+        Sxy[i] = S[2]
+    print(f'Sx = {Sx}')
+    print(f'Sy = {Sy}')
+    print(f'Sxy = {Sxy}')
+    return(Sx,Sy,Sxy)
+
 # Entrada de dados
 # Tipo de entrada de dados
 string = input('Qual o caminho e o nome do arquivo de modelo que você deseja abrir (nome do arquivo sem extensão? ')
@@ -51,33 +159,13 @@ list_B = []
 list_pos = []
 Kg = np.zeros((nglt,nglt))
 if tpel == 'triangle':
-    for i in range(nelem):
-        D = constitutiva(dados_analise[0],Mat[Inci[i,0],1],Mat[Inci[i,0],2])
-        n1 = Inci[i,1]
-        n2 = Inci[i,2]
-        n3 = Inci[i,3]
-        Ae = 1/2.*(Coord[n2,0]*Coord[n3,1]+Coord[n1,0]*Coord[n2,1]+Coord[n3,0]*Coord[n1,1]-Coord[n2,0]*Coord[n1,1]
-                   -Coord[n1,0]*Coord[n3,1]-Coord[n3,0]*Coord[n2,1])
-        if Ae < 0: 
-            print(f'Elemento {i}: Ae = {Ae}')
-            parada = input()
-        b = np.array([Coord[n2,1]-Coord[n3,1],(-1)*Coord[n1,1]+Coord[n3,1],Coord[n1,1]-Coord[n2,1]])
-        c = np.array([(-1)*Coord[n2,0]+Coord[n3,0],Coord[n1,0]-Coord[n3,0],(-1)*Coord[n1,0]+Coord[n2,0]])
-        B = 1/(2*Ae)*np.array([[b[0],0.,b[1],0.,b[2],0.],[0.,c[0],0.,c[1],0.,c[2]],[c[0],b[0],c[1],b[1],c[2],b[2]]])
-        list_B.append(B)
-        if dados_analise[0] == 'EPT':
-            t = Mat[Inci[i,0],0]
-        else:
-            t = 1.
-        Ke = t*Ae*np.dot(np.dot(np.transpose(B),D),B)
-        pos = [2*n1,2*n1+1,2*n2,2*n2+1,2*n3,2*n3+1]
-        list_pos.append(pos)
-        for j in range(6):
-            for k in range(6):
-                Kg[pos[j],pos[k]] += Ke[j,k]
-    # print(list_B)
-    # print(list_pos)
-    print(f'Matriz de rigidez global: {Kg}')
+    Kg,list_B,list_pos = rigT3()
+else:
+    if tpel == 'quad':
+        Kg,list_pos = rigQ4()
+# print(list_B)
+# print(list_pos)
+print(f'Matriz de rigidez global: {Kg}')
 
 # Reordenação da matriz de rigidez e vetor de forças
 Kgord = np.zeros((nglt,nglt))
@@ -91,7 +179,9 @@ for i in range(nglt):
 
 # Sub matriz de rigidez para solução do problema estático (K u = F) com inclusão das condições de contorno
 K = Kgord[:dim,:dim]
+print(f'K: {K}')
 Fl = Ford[:dim]
+print(Fl)
 
 # Solução estática
 dl = linalg.solve(K,Fl - np.dot(Kgord[:dim,dim:],dord[dim:]))
@@ -104,42 +194,6 @@ for i in range(nnos):
     Ux[i] = d[2*i]
     Uy[i] = d[2*i+1]
 
-# Criando um indexador de nós x elementos
-index_nos = []
-for i in range(nnos):
-    index_nos.append([])
-
-# Cálculo das tensões para elementos T3
-print('Tensões nos elementos:')
-list_S = []
-for i in range(nelem):
-    posel = list_pos[i]
-    ue = np.array([d[posel[0]],d[posel[1]],d[posel[2]],d[posel[3]],d[posel[4]],d[posel[5]]])
-    D = constitutiva(dados_analise[0],Mat[Inci[i,0],1],Mat[Inci[i,0],2])
-    B = list_B[i]
-    S = np.dot(D,np.dot(B,ue))
-    list_S.append(S)
-    print(f'Elemento {i}:')
-    print(f'Sx = {S[0]}')
-    print(f'Sy = {S[1]}')
-    print(f'Sxy = {S[2]}')
-    for j in range(Inci.shape[1]-1):
-        index_nos[Inci[i,j+1]].append(i)
-# Tensões médias nos nós para elemento T3
-Sx = np.zeros((nnos))
-Sy = np.zeros((nnos))
-Sxy = np.zeros((nnos))
-for i in range(nnos):
-    for j in index_nos[i]:
-        S += list_S[j]
-    S = 1./len(index_nos[i]) * S
-    Sx[i] = S[0]
-    Sy[i] = S[1]
-    Sxy[i] = S[2]
-print(f'Sx = {Sx}')
-print(f'Sy = {Sy}')
-print(f'Sxy = {Sxy}')
-
 # Plotagem da estrutura deformada
 pltmef.plotdef2D(Coord,Inci,d)
 
@@ -147,7 +201,15 @@ pltmef.plotdef2D(Coord,Inci,d)
 pltmef.plot_contor2D(tpel,Coord,Inci,Ux,'Ux')
 pltmef.plot_contor2D(tpel,Coord,Inci,Uy,'Uy')
 
-# Plotagem das tensões como gráficos de contorno
-pltmef.plot_contor2D(tpel,Coord,Inci,Sx,'Sx')
-pltmef.plot_contor2D(tpel,Coord,Inci,Sy,'Sy')
-pltmef.plot_contor2D(tpel,Coord,Inci,Sxy,'Sxy')
+# Criando um indexador de nós x elementos
+index_nos = []
+for i in range(nnos):
+    index_nos.append([])
+
+# Cálculo das tensões para elementos T3
+if tpel == 'triangle':
+    Sx,Sy,Sxy = stressT3()
+    # Plotagem das tensões como gráficos de contorno
+    pltmef.plot_contor2D(tpel,Coord,Inci,Sx,'Sx')
+    pltmef.plot_contor2D(tpel,Coord,Inci,Sy,'Sy')
+    pltmef.plot_contor2D(tpel,Coord,Inci,Sxy,'Sxy')
